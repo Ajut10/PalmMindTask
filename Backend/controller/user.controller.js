a7dcee786ee659a9006e6fb2cbd9206e1864e146
Ajut Dangol <ajud10gol@gmail.com>
2025-11-04 14:11:29 +0545

    user login and register complete with hashed and token complete

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,13 +1,14 @@
+const  {authenticateToken,generateToken} = require("../middleware/auth.js");
 const userModel = require("../model/user.model");
-
+const bcrypt = require("bcryptjs");
 module.exports.registerUser = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    // ✅ Correct: use .save() on the instance
-    const newUser = new userModel({ username, email, password });
+    const newUser = new userModel({ username, email, password:hashedPassword });
     await newUser.save();
-
+    
     res
       .status(201)
       .json({ message: "User registered successfully", user: newUser });
@@ -18,3 +19,39 @@ module.exports.registerUser = async (req, res) => {
       .json({ message: "Error registering user", error: error.message });
   }
 };
+
+module.exports.loginUser = async (req, res) => {
+  try{
+    const {email , password }= req.body;
+    const user = await userModel.findOne({email});
+    if(user|| (await bcrypt.compare(password, user.password))){
+      const token = generateToken(user);
+    res.status(200).json({message: "Login Successful",  token, user});
+      
+    }else{
+      res.status(401).json({message: "Invalid Credentials"});
+    }
+    
+  }catch(error){
+    console.error("Login Error:", error);
+    res.status(500).json({message: "Error logging in user", error: error.message});
+  }
+};
+
+
+
+module.exports.getUser= async (req, res) => {
+  try{
+    const {userId} = req.params;
+    const user = await userModel.findById(userId);
+    if(user){
+      res.status(200).json({message: "User Found", user: user});
+    }else{
+      res.status(404).json({message: "User Not Found"});
+    }
+  }catch(error){
+    console.error("Get User Error:", error);
+    res.status(500).json({message: "Error fetching user", error: error.message});
+  }
+}
+
--- Backend/router/user.routes.js
+++ Backend/router/user.routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require("../controller/user.controller");
 
 router.post("/register", userController.registerUser);
+router.post("/login", userController.loginUser);
+router.get("/getUser/:userId", userController.getUser);
 
 module.exports = router;
